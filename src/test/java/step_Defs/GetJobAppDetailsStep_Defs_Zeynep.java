d6073aa7950f2101b80935d1f39e95fb8b7cf62f
zeynep <zeynepykmc@gmail.com>
2024-01-17 12:34:44 +0000

    version2

---
+++ src/test/java/step_Defs/GetJobAppDetailsStep_Defs_Zeynep.java
@@ -0,0 +1,15 @@
+package step_Defs.Admin;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class GetJobAppDetailsStep_Defs_Zeynep {
+    @Given("Admin makes a get request by using job application ID")
+    public void admin_makes_a_get_request_by_using_job_application_id() {
+
+    }
+    @Then("Admin verifies that response status code is {int} and response body")
+    public void admin_verifies_that_response_status_code_is_and_response_body(Integer int1) {
+
+    }
+}
