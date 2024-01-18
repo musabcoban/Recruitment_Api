213f597771599095bc7b2d079da071e3fc1986ad
Musab <mcoban03@gmail.com>
2024-01-18 13:20:34 +0000

    21

--- src/test/java/runner/CukesRunner.java
+++ src/test/java/runner/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features ="src/test/resources/features",
         glue = "step_Defs",
         dryRun = false,
-        tags = "@msb2"
+        tags = "@msb"
 
 
 )
